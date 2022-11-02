e6cc194006c5838aff967a2ed0b31cb4b08c14d0
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-02 17:47:03 -0600

    fix: program closed after serving one coffee

--- options/make_coffee.py
+++ options/make_coffee.py
@@ -1,6 +1,29 @@
-def make_coffee(coffee):
+from dictionaries import coffee_types
+
+
+def make_coffee(coffee_type, c_water, c_milk, c_coffee):
     """
     receives what type of coffee the user wants and makes it
     :return:
     """
-    print("making coffee")
+
+    water_required = coffee_types.menu[coffee_type]['ingredients']['water']
+    milk_required = coffee_types.menu[coffee_type]['ingredients']['milk']
+    coffee_required = coffee_types.menu[coffee_type]['ingredients']['coffee']
+
+    new_water = c_water - water_required
+    new_milk = c_milk - milk_required
+    new_coffee = c_coffee - coffee_required
+
+    if new_water < 0:
+        print("Insufficient water")
+        return c_water, c_milk, c_coffee
+    elif new_milk < 0:
+        print("Insufficient milk")
+        return c_water, c_milk, c_coffee
+    elif new_coffee < 0:
+        print("Insufficient coffee")
+        return c_water, c_milk, c_coffee
+    else:
+        print(f"Making {coffee_type}")
+        return new_water, new_milk, new_coffee
