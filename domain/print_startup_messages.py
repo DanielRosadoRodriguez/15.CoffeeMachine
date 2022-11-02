ff7def2c67f32a0b0e03c5df9cc17a0aecabdc1e
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-02 11:43:40 -0600

    feat: added a module that prints the messages when the machine is turned on and when the machine is turned off

---
+++ domain/print_startup_messages.py
@@ -0,0 +1,19 @@
+wellcome_ascii = '''
+【﻿ｓｏｍｅ　ｃｏｆｆｅｅ】
+'''
+
+
+def print_startup_message():
+    """
+    function that prints the wellcome to the machine program
+    """
+    print("Wellcome to our Coffee Machine!")
+    print(wellcome_ascii)
+
+
+def print_turn_off():
+    """
+    Function that prints a message
+    that indicates that the machine is turned off
+    """
+    print("Turning off machine...")
