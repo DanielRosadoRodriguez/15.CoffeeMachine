67466aea63f99086b71e34581cbf6737636206f1
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-02 15:55:30 -0600

    feat: add print statement that automatically prints every coffee in the dictionary of coffees

--- domain/option_menu.py
+++ domain/option_menu.py
@@ -1,3 +1,6 @@
+from dictionaries import coffee_types
+
+
 def option_menu():
     """
     function that prints the option menu
@@ -5,4 +8,6 @@ def option_menu():
     transforms the input into a number and returns it
     :return: option
     """
-    print("What type of coffee do you want?\n - Espresso\n")
+    print(f"What type of coffee do you want?")
+    for i, coffee in enumerate(coffee_types.menu):
+        print(f"{i+1} - {coffee.title()}")
