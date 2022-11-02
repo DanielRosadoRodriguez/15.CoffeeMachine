41e50b52f409ac22dac64c483915ed9fe5e069d3
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-02 16:15:43 -0600

    feat: add loop that exits the program only when the user inputs "off"

--- main_coffee_machine.py
+++ main_coffee_machine.py
@@ -1,5 +1,13 @@
 from domain import print_startup_messages as pm
 from domain import option_menu as om
+from domain import get_user_input as gui
 
 pm.print_startup_message()
-om.option_menu()
+
+while True:
+    om.option_menu()
+    option = gui.get_user_option()
+    if option == False:
+        print("turning off machine...")
+        break
+
