4d490c6459c1056d841d8757b38337471cb93696
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-02 12:19:34 -0600

    feat: add function that displays the menu

---
+++ domain/option_menu.py
@@ -0,0 +1,8 @@
+def option_menu():
+    """
+    function that prints the option menu
+    receives the user input and validates it
+    transforms the input into a number and returns it
+    :return: option
+    """
+    print("What type of coffee do you want?\n - Espresso\n")
