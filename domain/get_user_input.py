dfd06f7f0bc589c9015d41f17d1d909f11848fb9
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-02 18:29:44 -0600

    feat: added input for coins

--- domain/get_user_input.py
+++ domain/get_user_input.py
@@ -1,5 +1,6 @@
 from options import make_coffee
 from options import print_report
+from dictionaries import coins as c
 
 coffee = 100
 milk = 200
@@ -11,7 +12,6 @@ def get_user_option():
     correct_input = False
     while not correct_input:
         option = input("Insert coffee that you want to chose: \n")
-        correct_input = True
         if option == '1':
             make_coffee.make_coffee(coffee_type='espresso')
             return True
@@ -29,3 +29,13 @@ def get_user_option():
         else:
             print("incorrect input")
             correct_input = False
+
+
+def get_coins():
+    pennys = int(input("How many Penny?: "))
+    nickels = int(input("How many Nickels?: "))
+    dimes = int(input("How many Dimes?: "))
+    quarters = int(input("How many Quarters?: "))
+    money = pennys*c.coins['penny'] + nickels*c.coins['nickel'] + dimes*c.coins['dime'] + quarters*c.coins['quarter']
+
+    return money
