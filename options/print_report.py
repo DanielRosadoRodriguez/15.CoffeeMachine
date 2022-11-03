c33834b433fb53915493a2376aae90d66c3485c4
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-02 18:01:09 -0600

    feat: add functionality that prints the ingredients

--- options/print_report.py
+++ options/print_report.py
@@ -1,5 +1,10 @@
+from dictionaries import ingredients
+
+
 def print_report():
     """Function that prints the report of
     the ingredients and money inside the machine"""
     print("--------")
     print("Report:")
+    for ingredient, quantity in ingredients.machine_ingredients.items():
+        print(f"{ingredient}: {quantity}")
