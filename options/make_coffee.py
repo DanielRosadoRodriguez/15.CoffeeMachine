7bf2b9a416a0537d466bedfcf6f574ad86c5ec36
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-02 18:06:59 -0600

    fix: changed way to access current values in the machine

--- options/make_coffee.py
+++ options/make_coffee.py
@@ -1,7 +1,8 @@
 from dictionaries import coffee_types
+from dictionaries import coffee_current_values
 
 
-def make_coffee(coffee_type, c_water, c_milk, c_coffee):
+def make_coffee(coffee_type):
     """
     receives what type of coffee the user wants and makes it
     :return:
@@ -11,19 +12,22 @@ def make_coffee(coffee_type, c_water, c_milk, c_coffee):
     milk_required = coffee_types.menu[coffee_type]['ingredients']['milk']
     coffee_required = coffee_types.menu[coffee_type]['ingredients']['coffee']
 
+    c_water = coffee_current_values.machine_ingredients['water']
+    c_milk = coffee_current_values.machine_ingredients['milk']
+    c_coffee = coffee_current_values.machine_ingredients['coffee']
+
     new_water = c_water - water_required
     new_milk = c_milk - milk_required
     new_coffee = c_coffee - coffee_required
 
     if new_water < 0:
         print("Insufficient water")
-        return c_water, c_milk, c_coffee
     elif new_milk < 0:
         print("Insufficient milk")
-        return c_water, c_milk, c_coffee
     elif new_coffee < 0:
         print("Insufficient coffee")
-        return c_water, c_milk, c_coffee
     else:
         print(f"Making {coffee_type}")
-        return new_water, new_milk, new_coffee
+        coffee_current_values.machine_ingredients['water'] = new_water
+        coffee_current_values.machine_ingredients['milk'] = new_milk
+        coffee_current_values.machine_ingredients['coffee'] = new_coffee
